c86946cfff85a2f6b8ef81deec0e5505ab0d83b5
RubenScheffer <60524485+Rubenscheffer@users.noreply.github.com>
2021-01-19 19:10:45 +0100

    Update Image_processing.py

--- Image_processing.py
+++ Image_processing.py
@@ -6,15 +6,18 @@ Created on Mon Jan 18 12:20:48 2021
 """
 from osgeo import gdal
 import matplotlib.pyplot as plt
-
+import numpy as np
 pathfile = r'C://Users/Ruben/Documents/Thesis/Data/Imagetest/'
 
 fp = pathfile + '20201228_033335_48_2424_3B_AnalyticMS_clip.tiff'
 
 image = gdal.Open(fp, gdal.GA_ReadOnly)
 
-# Load specific band
+# Load specific band (1-4)
 band = image.GetRasterBand(4)
 
+
+
 arr = band.ReadAsArray()
+print(np.max(arr))
 plt.imshow(arr)
