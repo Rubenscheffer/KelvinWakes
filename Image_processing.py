f519aaabcbcc027d3993cf5c49582e8d08bbeeb5
RubenScheffer <60524485+Rubenscheffer@users.noreply.github.com>
2021-01-19 12:58:01 +0100

    Update Image_processing.py

--- Image_processing.py
+++ Image_processing.py
@@ -4,16 +4,14 @@ Created on Mon Jan 18 12:20:48 2021
 
 @author: Ruben
 """
-
-import rasterio
-from rasterio.plot import show 
-import georaster
+from osgeo import gdal
 import matplotlib.pyplot as plt
 
 pathfile = r'C://Users/Ruben/Documents/Thesis/Data/Imagetest/'
 
 fp = pathfile + '20201228_033335_48_2424_3B_AnalyticMS_clip.tiff'
 
-img = georaster.MultiBandRaster(fp)
-
-
+dataset = gdal.Open(fp, gdal.GA_ReadOnly)
+band = dataset.GetRasterBand(4)
+arr = band.ReadAsArray()
+plt.imshow(arr)
