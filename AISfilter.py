8df9f34d5010b95028c8bc56747a807391d79ea4
RubenScheffer <60524485+Rubenscheffer@users.noreply.github.com>
2021-02-24 14:47:51 +0100

    Update AISfilter.py

--- AISfilter.py
+++ AISfilter.py
@@ -1,7 +1,7 @@
 "Code of 19/02/2021 meant to read and filter AIS Spire data"
 
 #%% Imports
-# import numpy as np
+import numpy as np
 # import pickle
 import pandas as pd
 import time
@@ -16,15 +16,56 @@ start_time = time.time()
 filepath = r'C:/Users/Ruben/Documents/Thesis/Data/AIS/AISdata.csv'
 
 data = pd.read_csv(filepath)
-headers = data.columns
-data_head = data.head()
+
+data.info()
+
+#%% Adding time columns
+
+time_column = data.loc[:, 'created_at']
+
+hours = np.zeros(len(time_column))
+date = np.zeros(len(time_column))
+minutes = np.zeros(len(time_column))
+
+
+for i in range(len(time_column)):
+    if time_column[i][8] == 0:
+        date[i] = int(time_column[i][9])
+    else:
+        date[i] = int(time_column[i][8:10])
+
+    if time_column[i][11] == 0:
+        hours[i] = int(time_column[i][12])
+    else:
+        hours[i] = int(time_column[i][11:13])
+    if time_column[i][14] == 0:
+        minutes[i] = int(time_column[i][15])
+    else:
+        minutes[i] = int(time_column[i][14:16])
+    if i % 10000 == 0:
+        print(i)
+
+data['date'] = date
+data['hours'] = hours
+data['minutes'] = minutes
+
+#Filter dataset by time
+
+data = data.loc[data['hours'] > 9]
+data = data.loc[data['hours'] < 14]
+
 
 #%% Filtering File
 
-columns_to_drop = ['created_at', 'eta', 'destination', 'status', 'maneuver']
-data.drop(columns_to_drop, inplace=True, axis='columns')
+#Dropping unneeded columns
+# columns_to_drop = ['created_at', 'eta', 'destination', 'status', 'maneuver']
+# data.drop(columns_to_drop, inplace=True, axis='columns')
 
-# Joining two measurement sets together
+
+#%% Sort by mmsi
+
+data = data.sort_values(by=['mmsi', 'msg_type'], ascending=False)
+
 
 #%% After filtering
 
@@ -44,6 +85,8 @@ data_head = data.head()
 # plt.show()
 
 #%% Final Message
+headers = data.columns
+data_head = data.head()
 
 end_time = time.time()
 execution_time = end_time - start_time
