1d60237e600616deefe71c8cffe648d229bef086
RubenScheffer <60524485+Rubenscheffer@users.noreply.github.com>
2021-03-16 12:11:35 +0100

    Update metadatareader.py

--- metadatareader.py
+++ metadatareader.py
@@ -25,7 +25,7 @@ def json_reader(file):
     output: geometry  list        n x 2 list of coordinates describing imagery area
             time      string      UTC time of image
     """
-    with open(path) as f:
+    with open(file) as f:
         data = json.load(f)
 
     geometry = data['geometry']['coordinates'][0]
@@ -34,15 +34,41 @@ def json_reader(file):
     return geometry, time
 
 
-
-for i, file in enumerate(files):
-    if file[-3:] == 'zip':
-        path = folder + file
-        zipfolder = zipfile.ZipFile(path, 'r')
-        zipfiles = zipfolder.namelist()
-        
-        for files in zipfiles:
-            if files[-9:] == 'data.json':
-                geom, time = json_reader(files)
-                print('check')
+def zip_search(file):
+    """
+    Searches folders and saves a list of all zip files within
     
+    input: file      string      Path to folder
+    
+    output: zips     list        List of zip files
+    """
+    
+    zips = []
+    
+    for i, doc in enumerate(file):
+        if doc[-3:] == 'zip':
+            path = folder + doc
+            zips.append(path)
+        
+    return zips
+
+
+def read_zip(zippath):
+    """
+    Read a single zip file and locate the metadata file within
+    
+    input: zippath      string      Path to zip file
+    
+    output: current_zip ZipFile     Zipfile object
+    """
+    current_zip = zipfile.ZipFile(zippath)
+    files_in_zip = current_zip.namelist()\
+    for i, file in enumerate(files_in_zip):
+        if file[-13:] == 'metadata.json':
+            print(file)
+            print(type(current_zip.read(file)))
+    return
+
+
+zippaths = zip_search(files)
+read_zip(zippaths[1])
